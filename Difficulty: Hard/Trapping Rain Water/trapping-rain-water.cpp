b5eb092e772f0975baa1214ffe8034cd1b12eac5
anand-satvik <157631341+anand-satvik@users.noreply.github.com>
2024-09-30 20:38:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -12,26 +12,23 @@ class Solution {
     long long trappingWater(vector<int> &arr) {
         // code here
         int n=arr.size();
-        long long lmax=0,rmax=0,total=0;
-        long long l=0,r=n-1;
+        int l=0;int r=n-1;int lmax=0,rmax=0;long long total=0;
         while(l<r)
         {
             if(arr[l]<=arr[r])
             {
-                if(lmax>arr[l])
-                {
-                    total+=lmax-arr[l];
-                }
-                else
+                if(lmax<=arr[l])
                 lmax=arr[l];
+                else
+                total+=lmax-arr[l];
                 l++;
             }
             else
             {
-                if(rmax>arr[r])
-                total+=rmax-arr[r];
-                else
+                if(rmax<=arr[r])
                 rmax=arr[r];
+                else
+                total+=rmax-arr[r];
                 r--;
             }
         }
